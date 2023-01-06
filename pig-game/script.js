f80ef922270535dd978046d03b68d695c8bbb095
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-01-06 10:10:12 +0530

    Update Logic

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,4 +1,6 @@
 'use strict';
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
@@ -17,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   // 1. Generate a random dice roll
@@ -31,8 +35,14 @@ btnRoll.addEventListener('click', function () {
   if (dice > 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
